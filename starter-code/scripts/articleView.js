0c035b4498ffc39ef4a7ca1949aaa05615617839
dougachrist <dougachrist@gmail.com>
2016-07-13 17:49:16 -0700

    add filter for category

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -40,9 +40,19 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
-  /* TODO: Just like we do for #author-filter above, we should also handle
+  /* TODO DONE: Just like we do for #author-filter above, we should also handle
   change events on the #category-filter element. Be sure to reset the
   #author-filter while you're at it! */
+  $('#category-filter').on('change', function() {
+    if ( $(this).val() ) {
+      $('#articles article').hide();
+      $('#articles article[data-category = "' + $(this).val() + '"]').show('slow');
+    } else {
+      $('#articles article').show();
+      $('.template').hide();
+    }
+    $('#author-filter').val('');
+  });
 };
 
 articleView.handleMainNav = function () {
@@ -74,3 +84,4 @@ articleView.setTeasers = function() {
 // TODO: Invoke all of the above functions (I mean, methods!):
 articleView.populateFilters();
 articleView.handleAuthorFilter();
+articleView.handleCategoryFilter();
